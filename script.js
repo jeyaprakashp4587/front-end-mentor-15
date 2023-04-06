1e7880472252c590bc2f242124776f6b40fc174b
jeyaprakashp4587 <126503394+jeyaprakashp4587@users.noreply.github.com>
2023-04-06 09:22:11 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function show(){
+    const home = document.getElementById("home");
+    const dom = document.getElementById("another-dom")
+
+    home.style.display = "none";
+    dom.style.display = "block";
+}
